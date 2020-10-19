6e872b8f9793c5cac52e09ada0483a8d8de3c4f7
Aleksandr Shtykov <aashtykov@dasreda.ru>
2020-10-19 18:36:48 +0300

    Updates

---
+++ precache-manifest.d0f0928fe92cabbcf043ebba26fdf3de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb60c4a81ce22264409bbeec3a07fa1e",
+    "url": "/aggregion-test/index.html"
+  },
+  {
+    "revision": "6b56f2a4f517609bf32b",
+    "url": "/aggregion-test/static/css/main.5753d29e.chunk.css"
+  },
+  {
+    "revision": "f1d9f5183a913d75c129",
+    "url": "/aggregion-test/static/js/2.c1c2ebf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/aggregion-test/static/js/2.c1c2ebf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b56f2a4f517609bf32b",
+    "url": "/aggregion-test/static/js/main.fff4a22b.chunk.js"
+  },
+  {
+    "revision": "5385861bd0f8f84ae7ea",
+    "url": "/aggregion-test/static/js/runtime-main.d58534ff.js"
+  }
+]);
